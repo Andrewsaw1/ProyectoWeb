6429396ba8b464424e5b2feaea4cc48df67eafd3
Andrewsaw1 <andrewsaw1@gmail.com>
2021-07-12 10:47:00 -0500

    cambios

--- index.php
+++ index.php
@@ -12,113 +12,11 @@ include('Controlador/ControllerListar.php');
     <link rel="stylesheet" href="Vista/icomoon/fonts/style.css">
     <link rel="stylesheet" href="Vista/css/estilosI.css">
     <title>Supersito</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 </head>
 
 <body>
-
-    <?php
-
-    $carrito_mio = $_SESSION['carrito'];
-    $_SESSION['carrito'] = $carrito_mio;
-    $totalcantidad=0;
-
-    // contamos nuestro carrito
-    if (isset($_SESSION['carrito'])) {
-        for ($i = 0; $i <= count($carrito_mio) - 1; $i++) {
-            if ($carrito_mio[$i] != NULL) {
-                $total_cantidad = $carrito_mio['cantidad'];
-                $total_cantidad++;
-                $totalcantidad += $total_cantidad;
-            }
-        }
-    }
-    ?>
-
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_cart" style="color: red;"><i class="fas fa-shopping-cart"></i> <?php echo $totalcantidad;?></a>
-                </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
-    <!-- END NAVBAR -->
-
-
-
-    <!-- Ventana CARRITO -->
-    <div class="modal fade" id="modal_cart" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Carrito</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-
-
-
-                    <div class="modal-body">
-                        <div>
-                            <div class="p-2">
-                                <ul class="list-group mb-3">
-                                    <?
-                                    if (isset($_SESSION['carrito'])) {
-                                        $total = 0;
-                                        for ($i = 0; $i <= count($carrito_mio) - 1; $i++) {
-                                            if ($carrito_mio[$i] != NULL) {
-                                    ?>
-                                                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                                    <div class="row col-12">
-                                                        <div class="col-6 p-0" style="text-align: left; color: #000000;">
-                                                            <h6 class="my-0">Cantidad: <?php echo $carrito_mio[$i]['cantidad'] ?> : <? echo $carrito_mio[$i]['titulo']; // echo substr($carrito_mio[$i]['titulo'],0,10); echo utf8_decode($titulomostrado)."..."; 
-                                                                                                                                    ?></h6>
-                                                        </div>
-                                                        <div class="col-6 p-0" style="text-align: right; color: #000000;">
-                                                            <span class="text-muted" style="text-align: right; color: #000000;"><? echo $carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad'];    ?> $</span>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                    <?
-                                                $total = $total + ($carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad']);
-                                            }
-                                        }
-                                    }
-                                    ?>
-                                    <li class="list-group-item d-flex justify-content-between">
-                                        <span style="text-align: left; color: #000000;">Total (MEX)</span>
-                                        <strong style="text-align: left; color: #000000;"> $<?php
-                                                                                            if (isset($_SESSION['carrito'])) {
-                                                                                                $total = 0;
-                                                                                                for ($i = 0; $i <= count($carrito_mio) - 1; $i++) {
-                                                                                                    if ($carrito_mio[$i] != NULL) {
-                                                                                                        $total = $total + ($carrito_mio[$i]['precio'] * $carrito_mio[$i]['cantidad']);
-                                                                                                    }
-                                                                                                }
-                                                                                            }
-                                                                                            echo $total; ?> MXN</strong>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <a type="button" class="btn btn-primary" href="Vista/html/index-pago.html">Pagar</a>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    <a type="button" class="btn btn-primary" href="borrarcarro.php">Vaciar carrito</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- END VENTANA CARRITO -->
 
     <div id="bar-nav">
         <div id="logo">
